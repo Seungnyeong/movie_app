42ed8b796af9fea72184797c0046fa3f214e29fc
mellow0410@naver.com <mellow0410@gmail.com>
2019-11-14 20:39:01 +0900

    second commit for react example

---
+++ src/classComponent.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class App extends React.Component{
+    // 매순간 setState를 호출할 때 마다 react가 새로운 state와 함께 render function을 호출한다.
+    constructor(props){
+        // constructor be called before render method();
+        super(props)
+        console.log("abcd")
+    }
+    state ={
+        count: 0
+    }
+    add = () =>{
+        this.setState(current => ({count : current.count + 1}))
+    }
+    minus = () => {
+        this.setState(current => ({count : current.count - 1}))
+    }
+    // component가 렌더링 되고 난 이후에 시작됨.
+    componentDidMount(){
+        console.log("compenent rendered")
+    }
+    //
+    componentDidUpdate(){
+        console.log("component updated")
+    }
+    componentWillUnmount(){
+        console.log("component unmonut")
+    }
+    
+    render(){
+
+        console.log("rendering!!")
+    return (
+           
+                <div>
+                    <h1> The number is {this.state.count}</h1>
+                    <button onClick={this.add}>Add</button>
+                    <button onClick={this.minus}>Minus</button>
+                </div>
+        
+        )
+    }
+}
+
+export default App;
